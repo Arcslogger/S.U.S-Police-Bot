f490f74e3793c405b0b3f3b31d8240d75ca19e80
Arcslogger <58961173+Arcslogger@users.noreply.github.com>
2021-04-10 21:58:08 -0400

    idk

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ import random
 import json
 
 prefixPath = 'data/prefix.json'
+wordList = ['sus', 'amogus', 'vent', 'red']
 
 def get_prefix(client, message):
     with open(prefixPath, 'r') as f:
@@ -40,17 +41,32 @@ async def changeprefix(ctx, prefix):
         json.dump(prefixes,f)
     await ctx.send(f'prefix is now {prefix}')
 
-#probably should change this to a general help command some time soon
+#all client.event stuff
 @client.event
 async def on_message(msg):
+    
     try:
+        #sus word
+        for x in wordList:
+            if x in msg.content.lower():
+                print("sus")
+                emoji = '\N{POSTBOX}'
+                await msg.add_reaction(emoji)
+
+        #help command
         if msg.mentions[0] == client.user:
             with open (prefixPath, 'r') as f:
                 prefixes = json.load(f)
-            pre = prefixes[str(msg.guild.id)]
-        await msg.channel.send(f'Prefix for this server is: {pre}')    
+                pre = prefixes[str(msg.guild.id)]
+            embed = discord.Embed(title = "List of Commands:")
+            embed.set_thumbnail(url="https://i.postimg.cc/yd4Jm0FV/1f8.png")
+            embed.add_field(name="List of sussy words:", value=f"{wordList}", inline=False)
+            embed.add_field(name="Change prefix (requires admin perms)", value="changePrefix", inline=False)
+            embed.set_footer(text=f"Prefix: {pre}")
+            await msg.channel.send(embed = embed)
+           
     except:
-        pass
+         pass
 
     await client.process_commands(msg)    
 
@@ -64,6 +80,8 @@ async def check(ctx):
 # @client.command()
 # async def leave(ctx):
 #     await ctx.guild.leave()
+
+
 
 with open('data/run.json') as f:
     token = json.load(f);

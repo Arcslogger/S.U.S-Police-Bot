25a5ebd9088d8ab8cc649bab452a94cf74dd883a
Arcslogger <58961173+Arcslogger@users.noreply.github.com>
2021-04-11 23:38:19 -0400

    bruh

--- bot.py
+++ bot.py
@@ -4,14 +4,16 @@ import datetime
 import asyncio
 import random
 import json
+import readData
 
-prefixPath = 'data/prefix.json'
+serverDataPath = 'data/serverData.json'
 wordList = ['sus', 'amogus', 'vent', 'red']
 
 def get_prefix(client, message):
-    with open(prefixPath, 'r') as f:
+    with open(serverDataPath, 'r') as f:
         prefixes = json.load(f);
-    return prefixes[str(message.guild.id)]
+    return prefixes[str(message.guild.id)]["prefix"]
+
 
 client = commands.Bot(command_prefix = get_prefix)
 
@@ -23,22 +25,14 @@ async def on_ready():
 #sets prefix after joining a server
 @client.event
 async def on_guild_join(guild):
+    readData.addServer(str(guild.id))
     print(f'joined server {guild.id}')
-    with open(prefixPath, 'r') as f:
-        prefixes = json.load(f)
-    prefixes[str(guild.id)] = '.'
-    with open(prefixPath, 'w') as f:
-        json.dump(prefixes,f)
 
 #change prefix
 @client.command()
 @commands.has_permissions(administrator = True)
 async def changeprefix(ctx, prefix):
-    with open(prefixPath, 'r') as f:
-        prefixes = json.load(f)
-    prefixes[str(ctx.guild.id)] = prefix
-    with open(prefixPath, 'w') as f:
-        json.dump(prefixes,f)
+    readData.changePrefix(prefix)
     await ctx.send(f'prefix is now {prefix}')
 
 #all client.event stuff
@@ -52,21 +46,20 @@ async def on_message(msg):
                 print("sus")
                 emoji = '\N{POSTBOX}'
                 await msg.add_reaction(emoji)
+                readData.addUser(str(msg.guild.id), str(msg.author.id), str(msg), -1)
 
         #help command
         if msg.mentions[0] == client.user:
-            with open (prefixPath, 'r') as f:
-                prefixes = json.load(f)
-                pre = prefixes[str(msg.guild.id)]
+
             embed = discord.Embed(title = "List of Commands:")
             embed.set_thumbnail(url="https://i.postimg.cc/yd4Jm0FV/1f8.png")
             embed.add_field(name="List of sussy words:", value=f"{wordList}", inline=False)
             embed.add_field(name="Change prefix (requires admin perms)", value="changePrefix", inline=False)
-            embed.set_footer(text=f"Prefix: {pre}")
+            embed.set_footer(text=f"Prefix: {readData.getPrefix(str(msg.guild.id))}")
             await msg.channel.send(embed = embed)
            
-    except:
-         pass
+    except IndexError:
+        pass
 
     await client.process_commands(msg)    
 
